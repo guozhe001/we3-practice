bf847514b3094d032a7368a6ef85e78092d88515
guozhe <guozhe@renmaitech.com>
2021-09-07 15:09:06 +0800

    添加多线程加快速度

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # This is a sample Python script.
 
+from concurrent.futures import ThreadPoolExecutor
+from concurrent import futures
+
 # Press ⇧F10 to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import eth_utils
@@ -11,7 +14,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/94bc20a138044cd7974fcd20b91d68ba'))
+w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/9b5196e976fe40babdbf6d83d497f074'))
 
 
 def print_transaction():
@@ -23,21 +26,30 @@ def print_transaction():
             print(k, v)
 
 
-def print_block():
-    all_burn_eth = 0
-    block_count = 0
-    from_block = 12965000
-    to_block = w3.eth.get_block_number()
-    for block_number in range(from_block, to_block + 1):
+def print_designated_block(from_block, end_block):
+    for block_number in range(from_block, end_block):
         # block = w3.eth.getBlock("0xdc0e0021f8b9c63c0b0217026eddb9ec9ad81156442fdfc8b6be90f38a131abb")
         block = w3.eth.getBlock(block_number)
         # for k, v in block.items():
         #     print(k, v)
         burn = eth_utils.from_wei(block.get("baseFeePerGas") * block.get("gasUsed"), "ether")
         print(f"block_number={block_number}, burn={burn}")
-        all_burn_eth += burn
-        block_count += 1
-    print(f"from_block={from_block}, to_block={to_block}, all_burn_eth={all_burn_eth}, new ether={block_count * 2}")
+
+
+executor = ThreadPoolExecutor(max_workers=10)
+
+
+def print_block():
+    from_block = 13079315
+    end_block = w3.eth.get_block_number()
+    fs = []
+    while from_block < end_block:
+        to_block = from_block + 1000
+        fs.append(executor.submit(print_designated_block, from_block, to_block))
+        # print(f"from_block={from_block}, to_block={to_block}")
+        from_block = to_block
+    for f in futures.as_completed(fs):
+        result = f.result()
 
 
 # Press the green button in the gutter to run the script.
